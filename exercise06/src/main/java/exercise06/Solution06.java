fb8063e3cb8548f06f0204dcb965fa36a9a410bc
Cristiam Enciso <encisocristiam@gmil.com>
2021-09-17 19:43:34 -0400

    update

---
+++ exercise06/src/main/java/exercise06/Solution06.java
@@ -0,0 +1,20 @@
+package exercise06;
+import java.util.Scanner;
+import java.util.Calendar;
+
+    public class Solution06 {
+        public static void main(String[] args) {
+            // Inititate scanner objects
+            Scanner in = new Scanner(System.in);
+            // Prompt users for input
+            System.out.print("What is your current age? ");
+            // Store value in string variables
+            String age = in.nextLine();
+            System.out.print("At what age would you like to retire? ");
+            String ageRetire = in.nextLine();
+            // Call retirement function
+        }
+    }
+
+
+
